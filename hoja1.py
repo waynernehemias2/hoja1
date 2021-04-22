cfb83a96386872d90b39f7ecd35e037d168a3c2c
waynernehemias2 <82613664+waynernehemias2@users.noreply.github.com>
2021-04-22 14:10:37 -0600

    Add files via upload

---
+++ hoja1.py
@@ -0,0 +1,21 @@
+print("Bienvenido")
+niveles = input("Ingrese número de niveles del triangulo rectangulo ")
+nivel=int(niveles)
+
+#Ejercicio1
+i=0
+var =("*")
+while i < nivel:
+    i = i+1 
+    print (var)
+    var=(var+" *")
+
+#Ejercicio2
+numero= input("Ingrese un número entero ")
+num = int(numero)
+if num > 1:
+    if (num % i) == 0:
+        print("No es primo ")
+    else:
+        print("Es primo ")    
+
